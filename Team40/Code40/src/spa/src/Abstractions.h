6f9748c0fe04b4de59323863eab597afc4d53722
PotatoCombat <39809238+PotatoCombat@users.noreply.github.com>
2021-08-30 15:43:06 +0800

    Add SPA abstractions
    This file should contain all our SPA constants.

---
+++ Team40/Code40/src/spa/src/Abstractions.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <string>
+
+using namespace std;
+
+typedef int Statement;
+typedef string Var;
+typedef string Const;
+typedef string Procedure;
+typedef string Pattern;
+
+typedef int VarIndex;
+typedef int ConstIndex;
+typedef int ProcedureIndex;
+typedef int StatementIndex;
