f6330c75c7e7040dbcc8fc52d713d20abcf924b8
fatin99 <fatinnbs@gmail.com>
2021-08-30 20:03:03 +0800

    Create class for AssignStatement

--- Team40/Code40/src/spa/src/AssignStatement.cpp
+++ Team40/Code40/src/spa/src/AssignStatement.cpp
@@ -7,16 +7,25 @@ class PrintStatement : public Statement {
 
 private:
 	Factor variable;
-	Expression expression; //TODO: Implement this
+	Expression expression;
 
 public:
 	void setVariable(Factor variable) { //this can only be a factor of type variable
 		this->variable = variable;
 	}
 
+	void setExpression(Expression expression) {
+		this->expression = expression;
+	}
+
 	Factor getVariable() {
 		return this->variable;
 	}
+
+	Expression getExpression() {
+		return this->expression;
+	}
+
 	StatementType getStatementType() {
 		return StatementType::ASSIGN;
 	}
