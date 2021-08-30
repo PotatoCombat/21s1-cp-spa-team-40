7c01d27c8ac87a9289a0c200d2b4eba6786abf44
Aakanksha Rai <rai.aakanksha@gmail.com>
2021-08-31 05:12:49 +0800

    Add follows and followsStar relationship tables

---
+++ Team40/Code40/src/spa/src/FollowsStarTable.h
@@ -0,0 +1,16 @@
+//
+// Created by Aakanksha on 31/08/2021.
+//
+
+#ifndef INC_21S1_CP_SPA_TEAM_40_FOLLOWSSTARTABLE_H
+#define INC_21S1_CP_SPA_TEAM_40_FOLLOWSSTARTABLE_H
+
+
+
+class FollowsStarTable {
+
+};
+
+
+
+#endif //INC_21S1_CP_SPA_TEAM_40_FOLLOWSSTARTABLE_H
