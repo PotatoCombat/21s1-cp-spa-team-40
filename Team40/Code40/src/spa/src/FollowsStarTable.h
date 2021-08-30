1fdb7f2012b57bbde2e5bb5d4b8db9f8fa1eae03
Aakanksha Rai <rai.aakanksha@gmail.com>
2021-08-31 05:16:22 +0800

    Add follows and follows star tables

--- Team40/Code40/src/spa/src/FollowsStarTable.h
+++ Team40/Code40/src/spa/src/FollowsStarTable.h
@@ -1,16 +1,84 @@
-//
-// Created by Aakanksha on 31/08/2021.
-//
+#pragma once
 
-#ifndef INC_21S1_CP_SPA_TEAM_40_FOLLOWSSTARTABLE_H
-#define INC_21S1_CP_SPA_TEAM_40_FOLLOWSSTARTABLE_H
-
-
+#include <map>
+#include <set>
+#include <Abstractions.h>
 
 class FollowsStarTable {
 
+public:
+    FollowsStarTable();
+    bool insertNextStatement(StatementIndex stmt1, StatementIndex stmt2);
+    set<StatementIndex> getNextStatements(StatementIndex stmt);
+    set<StatementIndex> getPreviousStatements(StatementIndex stmt);
+    bool followsStar(StatementIndex stmt1, StatementIndex stmt2);
+
+private:
+    //stmt2 follows stmt1 (i.e. key comes before stmts in value)
+    map<StatementIndex, set<StatementIndex>> followsStarTable;
+    //stmt2 is followed by stmt1 (i.e. stmts in value comes before key)
+    map<StatementIndex, set<StatementIndex>> followedByStarTable;
 };
 
+FollowsStarTable::FollowsStarTable() = default;
 
+bool insertNextStatement(StatementIndex stmt1, StatementIndex stmt2) {
+    if (followsStarTable.count(stmt1) <= 0) {
+        followsStarTable.insert(pair<StatementIndex, set<StatementIndex>>(stmt1, {}));
+    }
+    followsStarTable[stmt1].insert(stmt2);
 
-#endif //INC_21S1_CP_SPA_TEAM_40_FOLLOWSSTARTABLE_H
+    if (followedByStarTable.count(stmt2) <= 0) {
+        followedByStarTable.insert(pair<StatementIndex, set<StatementIndex>>(stmt2, {}));
+    }
+    followedByStarTable[stmt2].insert(stmt1);
+
+    //Adding follows*(prevStmts, stmt2)
+    if (followedByStarTable.count(stmt1) > 0) {
+        for (const StatementIndex& prevStmt : followedByStarTable[stmt1]) {
+            if (followsStarTable.count(prevStmt) <= 0) {
+                followsStarTable.insert(pair<StatementIndex, set<StatementIndex>>(prevStmt, {}));
+            }
+            followsStarTable[prevStmt].insert(stmt2);
+
+            if (followedByStarTable.count(stmt2) <= 0) {
+                followedByStarTable.insert(pair<StatementIndex, set<StatementIndex>>(stmt2, {}));
+            }
+            followedByStarTable[stmt2].insert(prevStmt);
+        }
+    }
+
+    //Adding follows*(stmt1, nextStmts)
+    if (followsStarTable.count(stmt2) > 0) {
+        for (const StatementIndex& nextStmt : followsStarTable[stmt2]) {
+            if (followsStarTable.count(stmt1) <= 0) {
+                followsStarTable.insert(pair<StatementIndex, set<StatementIndex>>(stmt1, {}));
+            }
+            followsStarTable[stmt1].insert(nextStmt);
+
+            if (followedByStarTable.count(nextStmt) <= 0) {
+                followedByStarTable.insert(pair<StatementIndex, set<StatementIndex>>(nextStmt, {}));
+            }
+            followedByStarTable[nextStmt].insert(stmt1);
+        }
+    }
+
+    return true;
+}
+
+set<StatementIndex> getNextStatements(StatementIndex stmt) {
+    return followsStarTable[stmt];
+}
+
+set<StatementIndex> getPreviousStatements(StatementIndex stmt) {
+    return followedByStarTable[stmt];
+}
+
+bool followsStar(StatementIndex stmt1, StatementIndex stmt2) {
+    set<StatementIndex> nextIndexes = followsStarTable[stmt1];
+    if (nextIndexes.find(stmt2) != nextIndexes.end()) {
+        return true;
+    }
+    return false;
+}
+
--- Team40/Code40/src/spa/src/FollowsTable.h
+++ Team40/Code40/src/spa/src/FollowsTable.h
@@ -1,16 +1,45 @@
-//
-// Created by Aakanksha on 31/08/2021.
-//
+#pragma once
 
-#ifndef INC_21S1_CP_SPA_TEAM_40_FOLLOWSTABLE_H
-#define INC_21S1_CP_SPA_TEAM_40_FOLLOWSTABLE_H
-
-
+#include <map>
+#include <Abstractions.h>
 
 class FollowsTable {
 
+public:
+    FollowsTable();
+    bool insertNextStatement(StatementIndex stmt1, StatementIndex stmt2);
+    StatementIndex getNextStatement(StatementIndex stmt);
+    StatementIndex getPreviousStatement(StatementIndex stmt);
+    bool follows(StatementIndex stmt1, StatementIndex stmt2);
+
+private:
+    //stmt2 follows stmt1 (i.e. key comes before value)
+    map<StatementIndex, StatementIndex> followsTable;
+    //stmt2 is followed by stmt1 (i.e. value comes before key)
+    map<StatementIndex, StatementIndex> followedByTable;
 };
 
+FollowsTable::FollowsTable() = default;
 
+bool insertNextStatement(StatementIndex stmt1, StatementIndex stmt2) {
+    followsTable[stmt1] = stmt2;
+    followedByTable[stmt2] = stmt1;
+    return true;
+}
 
-#endif //INC_21S1_CP_SPA_TEAM_40_FOLLOWSTABLE_H
+StatementIndex getNextStatement(StatementIndex stmt) {
+    return followsTable.at(stmt);
+}
+
+StatementIndex getPreviousStatement(StatementIndex stmt) {
+    return followedByTable.at(stmt);
+}
+
+bool follows(StatementIndex stmt1, StatementIndex stmt2) {
+    StatementIndex nextIndex = followsTable.at(stmt1);
+    if (nextIndex == stmt2) {
+        return true;
+    }
+    return false;
+}
+
