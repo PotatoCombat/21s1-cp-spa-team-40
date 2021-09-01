a585410af3104f93fd286708a9cd9190c29a0b73
fatin99 <fatinnbs@gmail.com>
2021-09-01 18:33:48 +0800

    Add header for SPA

---
+++ Team40/Code40/src/spa/src/SPA.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <PKB.h>
+#include <list>
+#include <string>
+using namespace std;
+
+class SPA {
+private:
+    // PKB pkb;
+
+public:
+    void processSource(string filename);
+    void processQuery(string query, list<string> &results);
+};
---
+++ Team40/Code40/src/spa/src/SourceProcessor/Parser.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+#include "PKB.h"
+#include "TNode.h"
+
+int Parse() { return 0; }
