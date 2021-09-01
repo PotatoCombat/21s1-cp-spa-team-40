562652c54f13ccd9c3ae902715314d14b063198b
Thai Duong <htduong292001@gmail.com>
2021-09-01 18:55:05 +0800

    TestEntity.cpp: Update test cases' names

--- Team40/Code40/src/unit_testing/src/TestEntity.cpp
+++ Team40/Code40/src/unit_testing/src/TestEntity.cpp
@@ -11,18 +11,17 @@ SynonymStatement synonymStatement("s2");
 NamedStatement namedStatement2("s2");
 NamedStatement namedStatement3("s1");
 
-
-TEST_CASE("QueryProcessor_Entity_IsSynomymEntity") {
+TEST_CASE("Entity: IsSynonymEntity") {
     REQUIRE(!namedStatement.IsSynonymEntity());
     REQUIRE(synonymStatement.IsSynonymEntity());
 }
 
-TEST_CASE("QueryProcessor_Entity_GetType") {
+TEST_CASE("Entity: GetType") {
     REQUIRE(namedStatement.GetType() == EntityType::STATEMENT);
     REQUIRE(synonymStatement.GetType() == EntityType::STATEMENT);
 }
 
-TEST_CASE("QueryProcessor_Entity_Equals") { 
+TEST_CASE("Entity: Equals") { 
     REQUIRE(namedStatement.Equals(namedStatement3));
     REQUIRE(!namedStatement.Equals(namedStatement2));
     REQUIRE(!synonymStatement.Equals(namedStatement2));
