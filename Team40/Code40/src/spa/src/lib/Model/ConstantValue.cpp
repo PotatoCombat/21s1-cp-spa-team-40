e3b280377b419ad30a1d4eef7d2135d2bcae3e64
fatin99 <fatinnbs@gmail.com>
2021-09-01 11:19:12 +0800

    Remove unused files

--- Team40/Code40/src/spa/src/lib/Model/ConstantValue.cpp
+++
@@ -1,3 +0,0 @@
-#include "ConstantValue.h"
-
-ConstantValue::ConstantValue(int value) : Factor(FactorType::CONSTANT, value) {}
--- Team40/Code40/src/spa/src/lib/Model/Expression.cpp
+++
@@ -1,6 +0,0 @@
-#include "Expression.h"
-
-Expression::Expression(ExpressionType expressionType, int value)
-    : expressionType(expressionType), Factor(FactorType::EXPRESSION, value) {}
-
-ExpressionType Expression::getExpressionType() { return this->expressionType; }
