b06e1cb5e7b1e2e153da797aaadb58d874014f30
yjskrs <e0323600@u.nus.edu>
2021-09-02 14:38:50 +0800

    Restructure preprocessor folder and draft preprocessor

---
+++ Team40/Code40/src/spa/src/query_processor/query_preprocessor/QueryPreprocessor.cpp
@@ -0,0 +1,12 @@
+#include "QueryPreprocessor.h"
+
+Query *QueryPreprocessor::preprocessQuery(string input) {
+    // read the input
+    // tokenize the input using a lexer
+    // syntax checker here
+    // parse the tokens using parser
+    // should return the entity / relationship items as in the component model
+    // class
+    // semantics checker here (?)
+    return Query();
+}
---
+++ Team40/Code40/src/spa/src/query_processor/query_preprocessor/QueryPreprocessor.h
@@ -0,0 +1,8 @@
+#include "../model/Query.h"
+
+using namespace std;
+
+class QueryPreprocessor {
+public:
+    Query *preprocessQuery(string input);
+};
