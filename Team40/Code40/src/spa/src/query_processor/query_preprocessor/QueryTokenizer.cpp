ec47b4f9a11ed3df75bca13ef1f9ca19164316e2
yjskrs <e0323600@u.nus.edu>
2021-09-02 17:38:56 +0800

    Add utility files for query preprocessor

---
+++ Team40/Code40/src/spa/src/query_processor/query_preprocessor/QueryTokenizer.cpp
@@ -0,0 +1,2 @@
+#include "QueryTokenizer.h"
+
---
+++ Team40/Code40/src/spa/src/query_processor/query_preprocessor/QueryTokenizer.h
@@ -0,0 +1,22 @@
+
+using namespace std;
+
+class QueryTokenizer {
+public:
+    // tokenize query into declaration part and the clauses part
+    // this returns DECLARATION and CLAUSES (vector?) tuple?
+
+    // then using the DECLARATION from step 1
+    // tokenize into DESIGN_ENTITY and SYNONYM
+
+    // then using the CLAUSES from step 1
+    // tokenize into SUCHTHAT_CLAUSE and PATTERN_CLAUSE
+
+    // tokenize SUCHTHAT_CLAUSE into (vector of) RELATION
+
+    // tokenize PATTERN_CLAUSE into SYNONYM, ENTITY_REF and EXPRESSION_SPEC
+
+    // tokenize EXPRESSION_SPEC into EXPRESSION
+
+private:
+};
