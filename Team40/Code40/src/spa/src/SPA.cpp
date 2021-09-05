0c4a62e11c5bbbf4944c3ffbc5f0a1d6b09d0cf3
fatin99 <fatinnbs@gmail.com>
2021-09-05 17:42:33 +0800

    Add internal storage for Variables and Constants

--- Team40/Code40/src/spa/src/SPA.cpp
+++ Team40/Code40/src/spa/src/SPA.cpp
@@ -1,10 +1,12 @@
 #include "SPA.h"
 #include "SourceProcessor/Line.h"
+#include "common/model/ConstantValue.h"
 #include "common/model/Program.h"
+#include "common/model/Variable.h"
 #include <fstream>
 #include <iostream>
 #include <string>
-
+#include <vector>
 using namespace std;
 
 void SPA::processSource(string filename) {
@@ -12,7 +14,10 @@ void SPA::processSource(string filename) {
     file.open(filename, ios::in);
     if (file.is_open()) {
         vector<Line> programLines = parser.parseFile(file);
+        // contains a list of procedures -> contains a list of statements
         Program program = parser.parseProgram(programLines);
+        vector<Variable> varLst = parser.getVarLst();
+        vector<ConstantValue> constLst = parser.getConstLst();
         int test = 0; // to bypass debugger
     } else {
         cout << "No such file";
