3b39e7f4b9bcd69c5f9e3c91b62c51294d2f2d19
Wallace <wallacelim97@gmail.com>
2021-09-08 23:35:41 +0800

    Add virtual functions in Statement to enable polymorphism

--- Team40/Code40/src/spa/src/common/include/common/model/statement/Statement.h
+++ Team40/Code40/src/spa/src/common/include/common/model/statement/Statement.h
@@ -1,5 +1,9 @@
 #pragma once
 
+#include "common/model/Variable.h"
+#include "common/model/condition/Condition.h"
+#include <vector>
+
 enum class StatementType { READ, PRINT, ASSIGN, CALL, WHILE, IF };
 
 class Statement {
@@ -14,5 +18,32 @@ public:
 
     // getters
     int getIndex();
+
     StatementType getStatementType();
+
+    // Overriden by: CallStatement
+    virtual string getProcName() { // return name instead of object to prevent
+                                   // circular dependencies
+        throw runtime_error("getProcName() is not implemented.");
+    }
+
+    // Overriden by: PrintStatement, ReadStatement, AssignStatement
+    virtual Variable getVariable() {
+        throw runtime_error("getVariable() is not implemented.");
+    };
+
+    // Overriden by: IfStatement, WhileStatement
+    virtual Condition getCondExpr() {
+        throw runtime_error("getCondExpr() is not implemented.");
+    }
+
+    // Overriden by: IfStatement, WhileStatement
+    virtual vector<Statement> getThenStmtLst() {
+        throw runtime_error("getThenStmtLst() is not implemented.");
+    }
+
+    // Overriden by: IfStatement
+    virtual vector<Statement> getElseStmtLst() {
+        throw runtime_error("getElseStmtLst() is not implemented.");
+    }
 };
