fe698dae33a8853e0feded2da2ad8c55bd7ec2c6
PotatoCombat <39809238+PotatoCombat@users.noreply.github.com>
2021-09-09 02:30:04 +0800

    Update SPA abstractions

--- Team40/Code40/src/spa/src/pkb/include/pkb/Abstractions.h
+++ Team40/Code40/src/spa/src/pkb/include/pkb/Abstractions.h
@@ -12,3 +12,8 @@ typedef int VarIndex;
 typedef int ConstIndex;
 typedef int ProcIndex;
 typedef int StmtIndex;
+
+VarIndex InvalidVarIndex = -1;
+ConstIndex InvalidConstIndex = -1;
+ProcIndex InvalidProcIndex = -1;
+StmtIndex InvalidStmtIndex = -1;
