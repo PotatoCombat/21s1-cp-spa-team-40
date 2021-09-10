b2e907c869aff4526cfee4fc33c162e176389bf4
yjskrs <e0323600@u.nus.edu>
2021-09-10 13:00:09 +0800

    Uncomment test case

--- Team40/Code40/src/unit_testing/src/query_processor/model/TestDesignEntityTypeHelper.cpp
+++ Team40/Code40/src/unit_testing/src/query_processor/model/TestDesignEntityTypeHelper.cpp
@@ -1,17 +1,17 @@
-//#include "catch.hpp"
-//
-//#include "DesignEntityTypeHelper.h"
-//
-//TEST_CASE("DesignEntityTypeHelper") {
-//	DesignEntityTypeHelper helper;
-//
-//	SECTION("test type to string map") {
-//		REQUIRE(helper.getType("stmt") == DesignEntityType::STMT);
-//		REQUIRE(helper.getType("assign") == DesignEntityType::ASSIGN);
-//		REQUIRE_THROWS_WITH(helper.getType("nonexistant"), "invalid design entity type");
-//	}
-//	
-//	SECTION("test string to type map") {
-//		REQUIRE(helper.getValue(DesignEntityType::IF) == "if");
-//	}
-//}
+#include "catch.hpp"
+
+#include "DesignEntityTypeHelper.h"
+
+TEST_CASE("DesignEntityTypeHelper") {
+	DesignEntityTypeHelper helper;
+
+	SECTION("test type to string map") {
+		REQUIRE(helper.getType("stmt") == DesignEntityType::STMT);
+		REQUIRE(helper.getType("assign") == DesignEntityType::ASSIGN);
+		REQUIRE_THROWS_WITH(helper.getType("nonexistant"), "invalid design entity type");
+	}
+	
+	SECTION("test string to type map") {
+		REQUIRE(helper.getValue(DesignEntityType::IF) == "if");
+	}
+}
