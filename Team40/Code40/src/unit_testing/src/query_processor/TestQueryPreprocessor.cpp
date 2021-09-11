5cd0df85070e55339867350cfe28d748106a2ddd
yjskrs <e0323600@u.nus.edu>
2021-09-11 09:24:22 +0800

    Add QueryPreprocessor tests

--- Team40/Code40/src/unit_testing/src/query_processor/TestQueryPreprocessor.cpp
+++ Team40/Code40/src/unit_testing/src/query_processor/TestQueryPreprocessor.cpp
@@ -6,38 +6,61 @@
 #include "Query.h"
 #include "QueryPreprocessor.h"
 
-struct TestQProcessor {
+struct TestQPreprocessor {
     static const string INPUT_1;
-    //static const string INPUT_2;
-    //static const string INPUT_3;
+    static const string INPUT_2;
+    static const string INPUT_3;
 };
 
-const string TestQProcessor::INPUT_1 =
+const string TestQPreprocessor::INPUT_1 =
     "stmt s;\nSelect s such that Follows(s, 4)";
-//const string TestQProcessor::INPUT_2 =
-//    "stmt s;\nSelect s";
-//const string TestQProcessor::INPUT_3 = 
-//    "stmt s;\nSelect p";
+const string TestQPreprocessor::INPUT_2 =
+    "stmt s;\nSelect s";
+const string TestQPreprocessor::INPUT_3 = 
+    "stmt s;\nSelect p";
 
 TEST_CASE("QueryPreprocessor") { 
-    Query expected;
-    Reference ret(DesignEntityType::STMT, ReferenceType::SYNONYM, "s");
-    Reference ref(DesignEntityType::STMT, ReferenceType::CONSTANT, "4");
-    Clause cls(ClauseType::FOLLOWS, ret, ref);
-    expected.setReturnReference(&ret);
-    expected.addClause(&cls);
-
     QueryPreprocessor qp;
-    Query actual;
     
     SECTION("test query created correctly") {
-        qp.preprocessQuery(TestQProcessor::INPUT_1, actual);
-        /*cout << (*actual.getClauses()[0]).getFirstReference()->getValue() << endl;
-        cout << (cls.getFirstReference()->getValue()) << endl;
-        cout << (*actual.getClauses()[0]).getSecondReference()->getValue() << endl;
-        cout << (cls.getSecondReference()->getValue()) << endl;*/
+        Query expected;
+        Query actual;
+        Reference ret(DesignEntityType::STMT, ReferenceType::SYNONYM, "s");
+        Reference ref(DesignEntityType::STMT, ReferenceType::CONSTANT, "4");
+        
+        SECTION("test 1") {
+            Clause cls(ClauseType::FOLLOWS, ret, ref);
+            expected.setReturnReference(&ret);
+            expected.addClause(&cls);
 
-        //REQUIRE((*actual.getClauses()[0]).equals(*cls));
-        REQUIRE((actual.getReturnReference()->equals(ret)));
+            qp.preprocessQuery(TestQPreprocessor::INPUT_1, actual);
+            /*cout << (*actual.getClauses()[0]).getFirstReference()->getValue() << endl;
+            cout << (cls.getFirstReference()->getValue()) << endl;
+            cout << (*actual.getClauses()[0]).getSecondReference()->getValue() << endl;
+            cout << (cls.getSecondReference()->getValue()) << endl;*/
+
+            REQUIRE((actual.getClauses()[0])->equals(cls));
+            REQUIRE((actual.getClauses()[0])->equals(*expected.getClauses()[0]));
+            REQUIRE((actual.getReturnReference()->equals(*expected.getReturnReference())));
+        }
+        
+        SECTION("test 2") {
+            expected.setReturnReference(&ret);
+            qp.preprocessQuery(TestQPreprocessor::INPUT_2, actual);
+
+            REQUIRE(actual.getClauses().size() == 0);
+            REQUIRE((actual.getReturnReference()->equals(*expected.getReturnReference())));
+        }
+    }
+
+    SECTION("test query not created due to missing return entity") {
+        Query expected;
+        Query actual;
+        Reference decl(DesignEntityType::STMT, ReferenceType::CONSTANT, "s");
+        Reference ret(DesignEntityType::STMT, ReferenceType::SYNONYM, "p");
+        expected.setReturnReference(&ret);
+
+        REQUIRE_THROWS(qp.preprocessQuery(TestQPreprocessor::INPUT_3, actual));
+        //REQUIRE(actual.getClauses().size() == 0);
     }
 }
