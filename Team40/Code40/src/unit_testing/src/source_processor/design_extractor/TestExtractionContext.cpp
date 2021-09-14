48d8dbf55531b3c1ca426b876ddf204ea4784c3d
fatin99 <fatinnbs@gmail.com>
2021-09-14 12:51:08 +0800

    Fix build errors

--- Team40/Code40/src/unit_testing/src/source_processor/design_extractor/TestExtractionContext.cpp
+++ Team40/Code40/src/unit_testing/src/source_processor/design_extractor/TestExtractionContext.cpp
@@ -1,82 +1,101 @@
 #include "catch.hpp"
 #include "source_processor/design_extractor/ExtractionContext.h"
 
-TEST_CASE("ExtractionContext: Creates correctly") {
-    REQUIRE(
-        !ExtractionContext::getInstance().getCurrentProcedure().has_value());
-    REQUIRE(ExtractionContext::getInstance()
-                .getParentContext()
-                .getAllEntities()
-                .empty());
-    REQUIRE(ExtractionContext::getInstance()
-                .getFollowsContext()
-                .getAllEntities()
-                .empty());
-    REQUIRE(!ExtractionContext::getInstance().getUsingStatement().has_value());
-    REQUIRE(
-        !ExtractionContext::getInstance().getModifyingStatement().has_value());
-}
+TEST_CASE("TestExtractionContext") {
+    ExtractionContext::getInstance().reset();
 
-TEST_CASE("ExtractionContext: Correctly pushes and pops Entities") {
-    const string PROC_NAME = "TEST_PROC";
-    Procedure procedure(PROC_NAME);
-    REQUIRE(
-        !ExtractionContext::getInstance().getCurrentProcedure().has_value());
-    ExtractionContext::getInstance().setCurrentProcedure(&procedure);
-    REQUIRE(ExtractionContext::getInstance().getCurrentProcedure().has_value());
-    REQUIRE(ExtractionContext::getInstance()
-                .getCurrentProcedure()
-                .value()
-                ->getName() == PROC_NAME);
-}
+    SECTION("Creates correctly") {
 
-TEST_CASE("ExtractionContext: Correctly sets and unsets statements") {
-    const string PROC_NAME = "TEST_PROC";
-    Statement callStatement(1, StatementType::CALL);
-    callStatement.setProcName(PROC_NAME);
-    REQUIRE(!ExtractionContext::getInstance().getUsingStatement().has_value());
-    ExtractionContext::getInstance().setUsingStatement(&callStatement);
-    REQUIRE(ExtractionContext::getInstance().getUsingStatement().has_value());
-    REQUIRE(ExtractionContext::getInstance().getUsingStatement().value() ==
-            &callStatement);
-}
+        REQUIRE(!ExtractionContext::getInstance()
+                     .getCurrentProcedure()
+                     .has_value());
+        REQUIRE(ExtractionContext::getInstance()
+                    .getParentContext()
+                    .getAllEntities()
+                    .empty());
+        REQUIRE(ExtractionContext::getInstance()
+                    .getFollowsContext()
+                    .getAllEntities()
+                    .empty());
+        REQUIRE(
+            !ExtractionContext::getInstance().getUsingStatement().has_value());
+        REQUIRE(!ExtractionContext::getInstance()
+                     .getModifyingStatement()
+                     .has_value());
+    }
 
-TEST_CASE(
-    "ExtractionContext: Correctly builds dependency tree for Procedures") {
-    const ProcName proc1 = "PROC_1";
-    const ProcName proc2 = "PROC_2";
-    const ProcName proc3 = "PROC_3";
-    const ProcName proc4 = "PROC_4";
-    const ProcName proc5 = "PROC_5";
+    SECTION("Correctly pushes and pops Entities") {
+        ExtractionContext::getInstance().reset();
+        const string PROC_NAME = "TEST_PROC";
+        Procedure procedure(PROC_NAME);
+        REQUIRE(!ExtractionContext::getInstance()
+                     .getCurrentProcedure()
+                     .has_value());
+        ExtractionContext::getInstance().setCurrentProcedure(&procedure);
+        REQUIRE(
+            ExtractionContext::getInstance().getCurrentProcedure().has_value());
+        REQUIRE(ExtractionContext::getInstance()
+                    .getCurrentProcedure()
+                    .value()
+                    ->getName() == PROC_NAME);
+    }
 
-    ExtractionContext::getInstance().addProcDependency(proc1, proc2);
-    ExtractionContext::getInstance().addProcDependency(proc1, proc5);
-    ExtractionContext::getInstance().addProcDependency(proc2, proc3);
-    ExtractionContext::getInstance().addProcDependency(proc2, proc4);
-    ExtractionContext::getInstance().addProcDependency(proc2, proc5);
-    ExtractionContext::getInstance().addProcDependency(proc4, proc5);
+    SECTION("Correctly sets and unsets statements") {
+        ExtractionContext::getInstance().reset();
+        const string PROC_NAME = "TEST_PROC";
+        Statement callStatement(1, StatementType::CALL);
+        callStatement.setProcName(PROC_NAME);
+        REQUIRE(
+            !ExtractionContext::getInstance().getUsingStatement().has_value());
+        ExtractionContext::getInstance().setUsingStatement(&callStatement);
+        REQUIRE(
+            ExtractionContext::getInstance().getUsingStatement().has_value());
+        REQUIRE(ExtractionContext::getInstance().getUsingStatement().value() ==
+                &callStatement);
+    }
 
-    auto deps1 = ExtractionContext::getInstance().getProcDependencies(proc1);
-    REQUIRE(deps1.count(proc2));
-    REQUIRE(!deps1.count(proc3));
-    REQUIRE(!deps1.count(proc4));
-    REQUIRE(deps1.count(proc5));
+    SECTION("Correctly builds dependency tree for Procedures") {
+        ExtractionContext::getInstance().reset();
+        const ProcName proc1 = "PROC_1";
+        const ProcName proc2 = "PROC_2";
+        const ProcName proc3 = "PROC_3";
+        const ProcName proc4 = "PROC_4";
+        const ProcName proc5 = "PROC_5";
 
-    auto deps2 = ExtractionContext::getInstance().getProcDependencies(proc2);
-    REQUIRE(!deps2.count(proc1));
-    REQUIRE(deps2.count(proc3));
-    REQUIRE(deps2.count(proc4));
-    REQUIRE(deps2.count(proc5));
+        ExtractionContext::getInstance().addProcDependency(proc1, proc2);
+        ExtractionContext::getInstance().addProcDependency(proc1, proc5);
+        ExtractionContext::getInstance().addProcDependency(proc2, proc3);
+        ExtractionContext::getInstance().addProcDependency(proc2, proc4);
+        ExtractionContext::getInstance().addProcDependency(proc2, proc5);
+        ExtractionContext::getInstance().addProcDependency(proc4, proc5);
 
-    auto deps3 = ExtractionContext::getInstance().getProcDependencies(proc3);
-    REQUIRE(deps3.empty());
+        auto deps1 =
+            ExtractionContext::getInstance().getProcDependencies(proc1);
+        REQUIRE(deps1.count(proc2));
+        REQUIRE(!deps1.count(proc3));
+        REQUIRE(!deps1.count(proc4));
+        REQUIRE(deps1.count(proc5));
 
-    auto deps4 = ExtractionContext::getInstance().getProcDependencies(proc4);
-    REQUIRE(!deps4.count(proc1));
-    REQUIRE(!deps4.count(proc2));
-    REQUIRE(!deps4.count(proc3));
-    REQUIRE(deps4.count(proc5));
+        auto deps2 =
+            ExtractionContext::getInstance().getProcDependencies(proc2);
+        REQUIRE(!deps2.count(proc1));
+        REQUIRE(deps2.count(proc3));
+        REQUIRE(deps2.count(proc4));
+        REQUIRE(deps2.count(proc5));
 
-    auto deps5 = ExtractionContext::getInstance().getProcDependencies(proc5);
-    REQUIRE(deps5.empty());
+        auto deps3 =
+            ExtractionContext::getInstance().getProcDependencies(proc3);
+        REQUIRE(deps3.empty());
+
+        auto deps4 =
+            ExtractionContext::getInstance().getProcDependencies(proc4);
+        REQUIRE(!deps4.count(proc1));
+        REQUIRE(!deps4.count(proc2));
+        REQUIRE(!deps4.count(proc3));
+        REQUIRE(deps4.count(proc5));
+
+        auto deps5 =
+            ExtractionContext::getInstance().getProcDependencies(proc5);
+        REQUIRE(deps5.empty());
+    }
 }
