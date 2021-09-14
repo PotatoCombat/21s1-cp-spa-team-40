c8dc6c96d55f4be2ba9dfbe2523a52b177b228d2
fatin99 <fatinnbs@gmail.com>
2021-09-14 13:01:04 +0800

    Fix build errors

--- Team40/Code40/src/spa/src/query_processor/include/query_processor/model/PatternClause.h
+++ Team40/Code40/src/spa/src/query_processor/include/query_processor/model/PatternClause.h
@@ -13,14 +13,8 @@ private:
     string pattern;
 
 public:
-<<<<<<< HEAD:Team40/Code40/src/spa/src/query_processor/include/query_processor/model/PatternClause.h
     PatternClause(Reference stmt, Reference var, string pattern);
-    Reference getStmt();
-    Reference getVar();
-=======
-    Pattern(Reference stmt, Reference var, string pattern);
     Reference *getStmt();
     Reference *getVar();
->>>>>>> master:Team40/Code40/src/spa/src/query_processor/include/query_processor/model/Pattern.h
     string getPattern();
 };
--- Team40/Code40/src/spa/src/query_processor/src/model/PatternClause.cpp
+++ Team40/Code40/src/spa/src/query_processor/src/model/PatternClause.cpp
@@ -3,8 +3,8 @@
 PatternClause::PatternClause(Reference stmt, Reference var, string pattern)
     : stmt(stmt), var(var), pattern(pattern) {}
 
-Reference PatternClause::getStmt() { return stmt; }
+Reference *PatternClause::getStmt() { return &this->stmt; }
 
-Reference PatternClause::getVar() { return var; }
+Reference *PatternClause::getVar() { return &this->var; }
 
 string PatternClause::getPattern() { return pattern; }
