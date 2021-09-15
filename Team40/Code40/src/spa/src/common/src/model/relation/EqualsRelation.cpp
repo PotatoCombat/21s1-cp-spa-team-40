cdaf8219b58367e63bebc63fb73c9fc513f69231
Wallace <wallacelim97@gmail.com>
2021-09-15 14:07:58 +0800

    Remove value, getValue() on Relation

--- Team40/Code40/src/spa/src/common/src/model/relation/EqualsRelation.cpp
+++ Team40/Code40/src/spa/src/common/src/model/relation/EqualsRelation.cpp
@@ -2,7 +2,3 @@
 
 EqualsRelation::EqualsRelation(Factor *leftFactor, Factor *rightFactor)
     : Relation(RelationType::EQUALS, leftFactor, rightFactor){};
-
-bool EqualsRelation::getValue() {
-    return getLeftFactor()->getValue() == getRightFactor()->getValue();
-}
