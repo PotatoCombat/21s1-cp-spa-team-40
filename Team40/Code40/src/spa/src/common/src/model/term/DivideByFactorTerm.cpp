3e861960270a55b4368674fde41f39407ec55a7d
Wallace <wallacelim97@gmail.com>
2021-09-15 14:08:09 +0800

    Remove value, getValue() on Term

--- Team40/Code40/src/spa/src/common/src/model/term/DivideByFactorTerm.cpp
+++ Team40/Code40/src/spa/src/common/src/model/term/DivideByFactorTerm.cpp
@@ -3,12 +3,4 @@
 DivideByFactorTerm::DivideByFactorTerm(Term *term, Factor *factor)
     : term(term), Term(TermType::MODULO_TERM_BY_FACTOR, factor){};
 
-int DivideByFactorTerm::getValue() {
-    if (!hasComputedValue) {
-        value = term->getValue() / getFactor()->getValue();
-        hasComputedValue = true;
-    }
-    return value;
-}
-
 Term *DivideByFactorTerm::getTerm() { return term; }
