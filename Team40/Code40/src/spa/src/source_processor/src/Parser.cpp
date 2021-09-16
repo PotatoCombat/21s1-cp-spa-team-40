e29da8d8b2f3fdb1116a2256e45343260df68321
fatin99 <fatinnbs@gmail.com>
2021-09-16 12:34:55 +0800

    Extract Parser.cpp

--- Team40/Code40/src/spa/src/source_processor/src/Parser.cpp
+++ Team40/Code40/src/spa/src/source_processor/src/Parser.cpp
@@ -1,1 +1,76 @@
 #include "source_processor/Parser.h"
+
+Program Parser::parseProgram(vector<Line> programLines, Program &program) {
+    Procedure *currProc = nullptr;
+    for (int i = 0; i < programLines.size(); i++) {
+        int currIndex = programLines[i].getIndex();
+        vector<string> currContent = programLines[i].getContent();
+        if (isProc(currContent)) {
+            // ... stmtLst '}'
+            if (i > 0) {
+                if (programLines[i - 1].getContent()[0] != "}") {
+                    throw("invalid program");
+                }
+            }
+            if (currProc != nullptr) {
+                // stmtLst: stmt+
+                if (currProc->getStmtLst().size() == 0) {
+                    throw("procedure should have at least one "
+                          "statement.");
+                }
+                program.addToProcLst(currProc);
+            }
+            ProcedureParser procParser(currContent);
+            currProc = procParser.parseProcedure();
+        } else if (!currContent.empty() && currContent[0] != "}" &&
+                   currContent[0] != "else") {
+            Statement *stmt =
+                parseStatement(currContent, currIndex, programLines, i);
+            currProc->addToStmtLst(stmt);
+        }
+    }
+    if (currProc != nullptr) {
+        // stmtLst: stmt+
+        if (currProc->getStmtLst().size() == 0) {
+            throw("procedure should have at least one statement.");
+        }
+        program.addToProcLst(currProc);
+    }
+    // program: procedure+
+    if (program.getProcLst().size() == 0) {
+        throw("program should have at least one procedure.");
+    }
+    return program;
+}
+
+Statement *Parser::parseStatement(vector<string> content, int index,
+                                  vector<Line> programLines,
+                                  int &programIndex) {
+    StatementParser stmtParser(content);
+    if (stmtParser.isReadStmt()) {
+        ReadStatementParser readParser(content, index);
+        return readParser.parseReadStatement();
+    } else if (stmtParser.isPrintStmt()) {
+        PrintStatementParser printParser(content, index);
+        return printParser.parsePrintStatement();
+    } else if (stmtParser.isCallStmt()) {
+        CallStatementParser callParser(content, index);
+        return callParser.parseCallStatement();
+    } else if (stmtParser.isAssignStmt()) {
+        AssignStatementParser assignParser(content, index);
+        return assignParser.parseAssignStatement();
+    } else if (stmtParser.isWhileStmt()) {
+        WhileStatementParser whileParser(content, index, programLines);
+        return whileParser.parseWhileStatement(programIndex);
+    } else if (stmtParser.isIfStmt()) {
+        IfStatementParser ifParser(content, index, programLines);
+        return ifParser.parseIfStatement(programIndex);
+    } else {
+        throw("invalid statement type");
+    }
+}
+
+bool Parser::isProc(vector<string> inputLine) {
+    return find(inputLine.begin(), inputLine.end(), "procedure") !=
+           inputLine.end();
+}
