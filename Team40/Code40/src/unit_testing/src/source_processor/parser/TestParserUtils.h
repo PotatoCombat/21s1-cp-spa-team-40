b9d527ad1a394340ed5ce99d4d7c9bcc63f92c02
PotatoCombat <39809238+PotatoCombat@users.noreply.github.com>
2021-09-18 00:44:51 +0800

    Setup SourceProcessor Parser unit tests

---
+++ Team40/Code40/src/unit_testing/src/source_processor/parser/TestParserUtils.h
@@ -0,0 +1,12 @@
+#include "catch.hpp"
+#include <iostream>
+#include <set>
+#include <string>
+#include <vector>
+using namespace std;
+
+struct TestParserUtils {
+public:
+    inline static int index = 1;
+    inline static vector<string> EMPTY_CONTENTS = {};
+};
