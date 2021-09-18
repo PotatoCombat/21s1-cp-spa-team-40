5f3ea61c20d8867fe4832683791e78f01a333112
fatin99 <fatinnbs@gmail.com>
2021-09-18 15:22:53 +0800

    Fix bug to allow 'else' to be used as a variable in assignstatements

--- Team40/Code40/src/spa/src/source_processor/include/source_processor/parser/Lexer.h
+++ Team40/Code40/src/spa/src/source_processor/include/source_processor/parser/Lexer.h
@@ -31,6 +31,7 @@ public:
     // special keywords
     bool isKeyword(string input);
     bool isAssignStmt(vector<string> content);
+    bool isStmt(vector<string> content);
 
     // special symbols
     bool isBracket(char input);
