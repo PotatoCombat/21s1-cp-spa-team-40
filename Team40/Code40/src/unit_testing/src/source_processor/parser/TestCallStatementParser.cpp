e5b44943fcd821f30e15495b5de662c813227d83
PotatoCombat <39809238+PotatoCombat@users.noreply.github.com>
2021-09-18 23:35:25 +0800

    Add unit test for CallStatementParser

---
+++ Team40/Code40/src/unit_testing/src/source_processor/parser/TestCallStatementParser.cpp
@@ -0,0 +1,28 @@
+#include "TestParserUtils.h"
+#include "source_processor/parser/CallStatementParser.h"
+
+TEST_CASE("CallStatementParser: parseCallStatement") {
+    int index = 1;
+    auto *actualStmt = CallStatementParser({"call", "x", ";", }, index).parseCallStatement();
+    auto testStmt = TestParserUtils::createCallStmt(index, "x");
+    REQUIRE(*actualStmt == testStmt);
+    delete actualStmt;
+}
+
+TEST_CASE("CallStatementParser: parseCallStatement - throws invalid variable name") {
+    int index = 1;
+    auto parser = CallStatementParser({"call", " x", }, index);
+    REQUIRE_THROWS(*parser.parseCallStatement());
+
+    parser = CallStatementParser({"call", "x ", }, index);
+    REQUIRE_THROWS(*parser.parseCallStatement());
+
+    parser = CallStatementParser({"call", "1x", }, index);
+    REQUIRE_THROWS(*parser.parseCallStatement());
+}
+
+TEST_CASE("CallStatementParser: parseCallStatement - throws invalid print statement") {
+    int index = 1;
+    auto parser = CallStatementParser({"call", "x", }, index);
+    REQUIRE_THROWS(*parser.parseCallStatement());
+}
