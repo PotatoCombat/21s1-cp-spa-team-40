ce971a4a6b16eeffd826c6bbb9c4e5c7a486080a
PotatoCombat <39809238+PotatoCombat@users.noreply.github.com>
2021-09-18 20:04:15 +0800

    Add unit test for PrintStatementParser

---
+++ Team40/Code40/src/unit_testing/src/source_processor/parser/TestPrintStatementParser.cpp
@@ -0,0 +1,28 @@
+#include "TestParserUtils.h"
+#include "source_processor/parser/PrintStatementParser.h"
+
+TEST_CASE("PrintStatementParser: parsePrintStatement") {
+    int index = 1;
+    auto *actualStmt = PrintStatementParser({"print", "x", ";", }, index).parsePrintStatement();
+    auto testStmt = TestParserUtils::createPrintStmt(index, "x");
+    REQUIRE(*actualStmt == testStmt);
+    delete actualStmt;
+}
+
+TEST_CASE("PrintStatementParser: parsePrintStatement - throws invalid variable name") {
+    int index = 1;
+    auto parser = PrintStatementParser({"print", " x", }, index);
+    REQUIRE_THROWS(*parser.parsePrintStatement());
+
+    parser = PrintStatementParser({"print", "x ", }, index);
+    REQUIRE_THROWS(*parser.parsePrintStatement());
+
+    parser = PrintStatementParser({"print", "1x", }, index);
+    REQUIRE_THROWS(*parser.parsePrintStatement());
+}
+
+TEST_CASE("PrintStatementParser: parsePrintStatement - throws invalid print statement") {
+    int index = 1;
+    auto parser = PrintStatementParser({"print", "x", }, index);
+    REQUIRE_THROWS(*parser.parsePrintStatement());
+}
