f2476881a23bb3e12158c644ba926c91a90846fe
yjskrs <e0323600@u.nus.edu>
2021-09-29 11:17:17 +0800

    Fix method calling in QueryPreprocessor and change order of parsing

--- Team40/Code40/src/spa/src/query_processor/src/QueryPreprocessor.cpp
+++ Team40/Code40/src/spa/src/query_processor/src/QueryPreprocessor.cpp
@@ -1,19 +1,22 @@
 #include "query_processor/QueryPreprocessor.h"
 
 void QueryPreprocessor::preprocessQuery(const string input, Query &q) {
-    pair<string, string> parts;
-    vector<DeclPair> declString;
-    string clauses;
-    string retString;
-    parts = tokenizer.separateDeclaration(input);
-    tokenizer.tokenizeDeclaration(parts.first, declString);
-    retString = tokenizer.tokenizeReturn(parts.second, clauses);
+    pair<string, string> parts = tokenizer.separateQueryString(input);
 
-    vector<Reference *> refList; // need to handle deleting of these objects
+    vector<DeclPair> declString;
+    string retString;
+    string clauses;
+
+    /*********** Parse declaration ***********/
+    tokenizer.tokenizeDeclarations(parts.first, declString);
+    vector<Reference *> refList;
     for (auto x : declString) {
         Reference *r = parser.parseDeclaration(x);
         refList.push_back(r);
     }
+
+    /*********** Parse return synonym ***********/
+    tokenizer.tokenizeReturnSynonym(parts.second, retString, clauses);
 
     int found = 0;
     for (auto x : refList) {
@@ -23,6 +26,7 @@ void QueryPreprocessor::preprocessQuery(const string input, Query &q) {
             break;
         }
     }
+
     if (!found) {
         throw ValidityError("return entity is undeclared");
     }
@@ -31,25 +35,29 @@ void QueryPreprocessor::preprocessQuery(const string input, Query &q) {
         return;
     }
 
-    // has clauses
-    vector<ClsTuple> clsString;
-    vector<PatTuple> patString;
+    /*********** Parse clauses ***********/
+    vector<ClsTuple> clsStrings;
+    vector<PatTuple> patStrings;
+    vector<WithTuple> withStrings;
 
-    tokenizer.tokenizeClause(clauses, clsString, patString);
+    tokenizer.tokenizeClauses(clauses, clsStrings, patStrings, withStrings);
 
     vector<Clause *> clsList;
-    for (auto x : clsString) {
+    for (auto x : clsStrings) {
         Clause *cls = parser.parseClause(x, refList);
         clsList.push_back(cls);
         q.addClause(cls);
     }
 
     vector<PatternClause *> patList;
-    for (auto x : patString) {
+    for (auto x : patStrings) {
         PatternClause *pat = parser.parsePattern(x, refList);
         patList.push_back(pat);
         q.addPattern(pat);
     }
+
+    // parse with clauses here
+    // ...
 
     for (int i = 0; i < refList.size(); ++i) {
         delete refList[i];
