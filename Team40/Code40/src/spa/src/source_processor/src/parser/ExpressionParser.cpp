556dd21cefe75a70143aba0d2d9586ef0b708633
fatin99 <fatinnbs@gmail.com>
2021-10-05 15:12:52 +0800

    Add check for no 0 at front of integer with more than one digit

--- Team40/Code40/src/spa/src/source_processor/src/parser/ExpressionParser.cpp
+++ Team40/Code40/src/spa/src/source_processor/src/parser/ExpressionParser.cpp
@@ -63,6 +63,9 @@ void ExpressionParser::checkValidBracket(string curr) {
 bool ExpressionParser::isInteger(string input) {
     // INTEGER: DIGIT+
     // constants are sequences of digits
+    if ((input.length() > 0) && (input[0] == 0)) {
+        return false;
+    }
     return find_if(input.begin(), input.end(), [](char c) { return !(isdigit(c)); }) == input.end();
 }
 
