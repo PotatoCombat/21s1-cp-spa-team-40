6b53214b6780ae5ac471b1819cdcd4268d982ae7
Huynh Thai Duong <htduong292001@gmail.com>
2021-10-09 13:56:50 +0800

    Delete PatternClause

--- Team40/Code40/src/spa/src/query_processor/src/model/PatternClause.cpp
+++
@@ -1,7 +0,0 @@
-#include "query_processor/model/PatternClause.h"
-
-PatternClause::PatternClause(Reference stmt, Reference var, string pattern, bool exactMatch)
-    : Clause(ClauseType::PATTERN, stmt, var, pattern, exactMatch) {}
-
-PatternClause::PatternClause(Reference stmt, Reference var)
-    : Clause(ClauseType::PATTERN, stmt, var) {}
