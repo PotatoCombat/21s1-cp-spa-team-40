a8012ce310b1f22b7911a3f163ecbf00bce23a62
yjskrs <e0323600@u.nus.edu>
2021-10-14 18:09:14 +0800

    Add tests

--- Team40/Code40/src/unit_testing/src/query_processor/TestQueryProcessor.cpp
+++ Team40/Code40/src/unit_testing/src/query_processor/TestQueryProcessor.cpp
@@ -5,11 +5,24 @@
 #include "query_processor/QueryProcessor.h"
 
 TEST_CASE("QueryProcessor: Select BOOLEAN = TRUE") {
-    const string SELECT_BOOLEAN = "Select BOOLEAN";
-
     QueryProcessor qp(nullptr);
     list<string> results;
 
+    string SELECT_BOOLEAN = "Select BOOLEAN";
+    qp.processQuery(SELECT_BOOLEAN, results);
+
+    REQUIRE(results.size() == 1);
+    REQUIRE(results.front() == "TRUE");
+
+    results.clear();
+    SELECT_BOOLEAN = "stmt s; Select BOOLEAN";
+    qp.processQuery(SELECT_BOOLEAN, results);
+
+    REQUIRE(results.size() == 1);
+    REQUIRE(results.front() == "TRUE");
+
+    results.clear();
+    SELECT_BOOLEAN = "Select BOOLEAN with 10 = 10";
     qp.processQuery(SELECT_BOOLEAN, results);
 
     REQUIRE(results.size() == 1);
@@ -17,13 +30,19 @@ TEST_CASE("QueryProcessor: Select BOOLEAN = TRUE") {
 }
 
 TEST_CASE("QueryProcessor: Select BOOLEAN = FALSE") {
-    // invalid attribute
-    const string SELECT_BOOLEAN =
-        "assign a; Select BOOLEAN with a.procName = 10";
-
     QueryProcessor qp(nullptr);
     list<string> results;
 
+    // invalid attribute
+    string SELECT_BOOLEAN = "assign a; Select BOOLEAN with a.varName = 10";
+    qp.processQuery(SELECT_BOOLEAN, results);
+
+    REQUIRE(results.size() == 1);
+    REQUIRE(results.front() == "FALSE");
+
+    results.clear();
+    // invalid type
+    SELECT_BOOLEAN = "assign a; Select BOOLEAN with \"variable\" = 10";
     qp.processQuery(SELECT_BOOLEAN, results);
 
     REQUIRE(results.size() == 1);
