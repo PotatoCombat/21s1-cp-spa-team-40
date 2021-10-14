146e39f38644083b3454741614299979eef84f59
yjskrs <e0323600@u.nus.edu>
2021-10-14 18:13:07 +0800

    Add test method

--- Team40/Code40/src/unit_testing/src/query_processor/TestQueryProcessor.cpp
+++ Team40/Code40/src/unit_testing/src/query_processor/TestQueryProcessor.cpp
@@ -4,29 +4,36 @@
 
 #include "query_processor/QueryProcessor.h"
 
+struct TestQueryProcessor {
+    static void requireResultsTrue(list<string> results) {
+        REQUIRE(results.size() == 1);
+        REQUIRE(results.front() == "TRUE");
+    };
+    static void requireResultsFalse(list<string> results) {
+        REQUIRE(results.size() == 1);
+        REQUIRE(results.front() == "FALSE");
+    };
+};
+
 TEST_CASE("QueryProcessor: Select BOOLEAN = TRUE") {
     QueryProcessor qp(nullptr);
     list<string> results;
 
     string SELECT_BOOLEAN = "Select BOOLEAN";
     qp.processQuery(SELECT_BOOLEAN, results);
-
-    REQUIRE(results.size() == 1);
-    REQUIRE(results.front() == "TRUE");
+    TestQueryProcessor::requireResultsTrue(results);
 
     results.clear();
+
     SELECT_BOOLEAN = "stmt s; Select BOOLEAN";
     qp.processQuery(SELECT_BOOLEAN, results);
-
-    REQUIRE(results.size() == 1);
-    REQUIRE(results.front() == "TRUE");
+    TestQueryProcessor::requireResultsTrue(results);
 
     results.clear();
+
     SELECT_BOOLEAN = "Select BOOLEAN with 10 = 10";
     qp.processQuery(SELECT_BOOLEAN, results);
-
-    REQUIRE(results.size() == 1);
-    REQUIRE(results.front() == "TRUE");
+    TestQueryProcessor::requireResultsTrue(results);
 }
 
 TEST_CASE("QueryProcessor: Select BOOLEAN = FALSE") {
@@ -36,15 +43,12 @@ TEST_CASE("QueryProcessor: Select BOOLEAN = FALSE") {
     // invalid attribute
     string SELECT_BOOLEAN = "assign a; Select BOOLEAN with a.varName = 10";
     qp.processQuery(SELECT_BOOLEAN, results);
-
-    REQUIRE(results.size() == 1);
-    REQUIRE(results.front() == "FALSE");
+    TestQueryProcessor::requireResultsFalse(results);
 
     results.clear();
+
     // invalid type
     SELECT_BOOLEAN = "assign a; Select BOOLEAN with \"variable\" = 10";
     qp.processQuery(SELECT_BOOLEAN, results);
-
-    REQUIRE(results.size() == 1);
-    REQUIRE(results.front() == "FALSE");
+    TestQueryProcessor::requireResultsFalse(results);
 }
