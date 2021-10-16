6714b1392bc02aa8a31c2f36b4929ed80669d516
PotatoCombat <39809238+PotatoCombat@users.noreply.github.com>
2021-10-16 18:25:10 +0800

    Add Next relationship handler

---
+++ Team40/Code40/src/spa/src/query_processor/src/relationship_handler/NextHandler.cpp
@@ -0,0 +1,29 @@
+#include "query_processor/relationship_handler/NextHandler.h"
+
+NextHandler::NextHandler(Clause* clause, PKB* pkb)
+    : ClauseHandler(clause, pkb, ClauseType::FOLLOWS) {
+    validDesType1 = &ClauseHandler::STMT_DES_SET;
+    validDesType2 = &ClauseHandler::STMT_DES_SET;
+    validRefType1 = &ClauseHandler::ALL_VALID_REF;
+    validRefType2 = &ClauseHandler::ALL_VALID_REF;
+}
+
+set<string> NextHandler::getR1ClauseR2(string r2) {
+    set<string> res;
+    for (int i : pkb->getNextLines(stoi(r2))) {
+        res.insert(to_string(i));
+    }
+    return res;
+}
+
+set<string> NextHandler::getR2ClausedR1(string r1) {
+    set<string> res;
+    for (int i : pkb->getPreviousLines(stoi(r1))) {
+        res.insert(to_string(i));
+    }
+    return res;
+}
+
+bool NextHandler::isR1ClauseR2(string r1, string r2) {
+    return pkb->next(stoi(r1), stoi(r2));
+}
