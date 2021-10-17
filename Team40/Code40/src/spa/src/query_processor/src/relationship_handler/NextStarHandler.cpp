0d42f1cb91e788a8dfa160f78052717b98a6d946
PotatoCombat <39809238+PotatoCombat@users.noreply.github.com>
2021-10-18 01:29:50 +0800

    Optimise Next Star algorithm
    - Uses optimised BFS
    - Fixes incorrect result for Next*(s, s) queries

--- Team40/Code40/src/spa/src/query_processor/src/relationship_handler/NextStarHandler.cpp
+++ Team40/Code40/src/spa/src/query_processor/src/relationship_handler/NextStarHandler.cpp
@@ -11,7 +11,6 @@ NextStarHandler::NextStarHandler(Clause* clause, PKB* pkb)
 set<string> NextStarHandler::getR1ClauseR2(string r2) {
     queue<ProgLineIndex> open;
     unordered_set<ProgLineIndex> visited;
-    bool repeatedR2 = false;
 
     ProgLineIndex line2 = stoi(r2);
     ProgLineIndex curr;
@@ -22,25 +21,19 @@ set<string> NextStarHandler::getR1ClauseR2(string r2) {
         curr = open.front();
         open.pop();
 
-        visited.insert(curr); // Mark current as visited
-
         // Explore neighbours
         for (ProgLineIndex i : pkb->getPreviousLines(curr)) {
             // Only add neighbours that haven't been visited
             if (visited.find(i) == visited.end()) {
+                visited.insert(i); // Mark current as visited
                 open.push(i);
-            }
-            else if (i == line2) {
-                repeatedR2 = true;
             }
         }
     }
 
     set<string> res;
     for (ProgLineIndex i : visited) {
-        if (i != line2 || repeatedR2) {
-            res.insert(to_string(i));
-        }
+        res.insert(to_string(i));
     }
 
     return res;
@@ -49,8 +42,6 @@ set<string> NextStarHandler::getR1ClauseR2(string r2) {
 set<string> NextStarHandler::getR2ClausedR1(string r1) {
     queue<ProgLineIndex> open;
     unordered_set<ProgLineIndex> visited;
-
-    bool repeatedR1 = false;
 
     ProgLineIndex line1 = stoi(r1);
     ProgLineIndex curr;
@@ -61,25 +52,20 @@ set<string> NextStarHandler::getR2ClausedR1(string r1) {
         curr = open.front();
         open.pop();
 
-        visited.insert(curr); // Mark current as visited
 
         // Explore neighbours
         for (ProgLineIndex i : pkb->getNextLines(curr)) {
             // Only add neighbours that haven't been visited
             if (visited.find(i) == visited.end()) {
+                visited.insert(i); // Mark current as visited
                 open.push(i);
-            }
-            else if (i == line1) {
-                repeatedR1 = true;
             }
         }
     }
 
     set<string> res;
     for (ProgLineIndex i : visited) {
-        if (i != line1 || repeatedR1) {
-            res.insert(to_string(i));
-        }
+        res.insert(to_string(i));
     }
 
     return res;
@@ -99,16 +85,15 @@ bool NextStarHandler::isR1ClauseR2(string r1, string r2) {
         curr = open.front();
         open.pop();
 
-        if (curr == line2) {
-            return true;
-        }
-
-        visited.insert(curr); // Mark current as visited
-
         // Explore neighbours
         for (ProgLineIndex i : pkb->getNextLines(curr)) {
+            if (i == line2) {
+                return true;
+            }
+
             // Only add neighbours that haven't been visited
             if (visited.find(i) == visited.end()) {
+                visited.insert(i); // Mark current as visited
                 open.push(i);
             }
         }
