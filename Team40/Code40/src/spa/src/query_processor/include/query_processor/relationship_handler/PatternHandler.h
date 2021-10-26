ffcae1aeb63a868eb5119ceaf8beb227eed5256c
Huynh Thai Duong <htduong292001@gmail.com>
2021-10-26 10:00:37 +0800

    Update PatternHandler header

--- Team40/Code40/src/spa/src/query_processor/include/query_processor/relationship_handler/PatternHandler.h
+++ Team40/Code40/src/spa/src/query_processor/include/query_processor/relationship_handler/PatternHandler.h
@@ -8,17 +8,25 @@
 
 #include "query_processor/Result.h"
 #include "query_processor/model/Clause.h"
+#include "query_processor/relationship_handler/ClauseHandler.h"
 #include "query_processor/relationship_handler/ClauseHandlerError.h"
 #include "query_processor/relationship_handler/ModifiesStmtHandler.h"
 #include "query_processor/model/DesignEntityTypeHelper.h"
 
 using namespace std;
 
-class PatternHandler {
+class PatternHandler;
+// typedef function pointers declared after the declaration of the class
+typedef set<int> (PatternHandler::*GetPatternStmtsFunc)(string, vector<string>);
+typedef bool (PatternHandler::*HasPatternFunc)(int, string, vector<string>);
+
+class PatternHandler : public ClauseHandler {
 private:
-    Clause *patternClause;
-    PKB *pkb;
-    void validate();
+    set<string> getR1ClauseR2(string r2) override;
+    set<string> getR2ClausedR1(string r1) override;
+    bool isR1ClauseR2(string r1, string r2) override;
+
+    void validate() override;
 
     // Wrappers for PKB functions
     set<int> getPartialAssignPatternStmts(string var, vector<string> pattern);
@@ -33,8 +41,13 @@ private:
     bool ifPattern(int stmt, string var, vector<string> pattern);
     bool whilePattern(int stmt, string var, vector<string> pattern);
 
+    /* Choose suitable function to use
+    * @param getPatternStmts, hasPattern: pointers to function pointer
+    */
+    void getFunctions(
+        GetPatternStmtsFunc &getPatternStmts,
+        HasPatternFunc &hasPattern);
+
 public:
-    PatternHandler(Clause *patternClause, PKB *pkb)
-        : patternClause(patternClause), pkb(pkb) {}
-    Result eval();
+    PatternHandler(Clause *patternClause, PKB *pkb);
 };
