624dfd6e89df935a15683fed12a9b71a4ac6ec66
Huynh Thai Duong <htduong292001@gmail.com>
2021-10-27 19:11:35 +0800

    Split QueryOptimizer::optimize into smaller functions

--- Team40/Code40/src/spa/src/query_processor/include/query_processor/QueryOptimizer.h
+++ Team40/Code40/src/spa/src/query_processor/include/query_processor/QueryOptimizer.h
@@ -3,6 +3,7 @@
 #include <algorithm>
 #include <map>
 #include <set>
+#include <stdexcept>
 #include <string>
 #include <vector>
 
@@ -13,6 +14,30 @@
 using namespace std;
 
 class QueryOptimizer {
+private:
+    static const int INVALID_IDX = -1;
+
+    // Returns a list of <group idx, group size>
+    static vector<pair<int, int>> formGroups(vector<Clause *> &unsortedClauses,
+                                             map<string, int> &refToGroup);
+
+    /* Updates group size of the 2 groups connected by cls
+    * Returns a vector containing idx1, idx2 and newGroupIdx
+    */ 
+    static vector<int> updateGroupSize(Clause *cls,
+                                       vector<pair<int, int>> &groupSizes,
+                                       map<string, int> &refToGroup);
+
+    // merges 2 groups with idx1 and idx2 that are connected by cls into a new group with newGroupIdx
+    static void mergeTwoGroups(int newGroupIdx, int idx1, int idx2, Clause *cls,
+                               map<string, int> &refToGroup);
+
+    static vector<Clause *> reorderClause(vector<Clause *> &unsortedClauses,
+                                          vector<pair<int, int>> &groupSizes,
+                                          map<string, int> &refToGroup);
+    
+    static void populateNewQueryObject(vector<Clause *> sortedClauses, Query &before, Query &after);
+
 public:
     static void optimize(Query &before, Query &after);
 };
