9df37cfa5bb9f701ba8d2237a484beecf5d0cc13
fatin99 <fatinnbs@gmail.com>
2021-10-28 19:41:37 +0800

    Remove all magic numbers

--- Team40/Code40/src/spa/src/source_processor/include/source_processor/parser/Tokens.h
+++ Team40/Code40/src/spa/src/source_processor/include/source_processor/parser/Tokens.h
@@ -4,50 +4,73 @@ using namespace std;
 
 class Tokens {
 public:
-    static constexpr string_view KEYWORD_PROCEDURE = "procedure";
-    static constexpr string_view KEYWORD_ELSE = "else";
-    static constexpr string_view KEYWORD_READ = "read";
-    static constexpr string_view KEYWORD_PRINT = "print";
-    static constexpr string_view KEYWORD_CALL = "call";
-    static constexpr string_view KEYWORD_WHILE = "while";
-    static constexpr string_view KEYWORD_IF = "if";
-    static constexpr string_view KEYWORD_THEN = "then";
-    static constexpr string_view SYMBOL_OPEN_BRACKET = "(";
-    static constexpr string_view SYMBOL_CLOSE_BRACKET = ")";
-    static constexpr string_view SYMBOL_OPEN_BRACE = "{";
-    static constexpr string_view SYMBOL_CLOSE_BRACE = "}";
-    static constexpr string_view SYMBOL_ASSIGN = "=";
-    static constexpr string_view SYMBOL_AMPERSAND = "&";
-    static constexpr string_view SYMBOL_BAR = "|";
-    static constexpr string_view SYMBOL_NOT = "!";
-    static constexpr string_view SYMBOL_LESS_THAN = "<";
-    static constexpr string_view SYMBOL_GREATER_THAN = ">";
-    static constexpr string_view SYMBOL_PLUS = "+";
-    static constexpr string_view SYMBOL_MINUS = "-";
-    static constexpr string_view SYMBOL_MULTIPLY = "*";
-    static constexpr string_view SYMBOL_DIVIDE = "/";
-    static constexpr string_view SYMBOL_MODULO = "%";
-    static constexpr string_view SYMBOL_SEMICOLON = ";";
-    static constexpr string_view SYMBOL_LESS_THAN_OR_EQUALS_TO = "<=";
-    static constexpr string_view SYMBOL_GREATER_THAN_OR_EQUALS_TO = ">=";
-    static constexpr string_view SYMBOL_EQUALS = "==";
-    static constexpr string_view SYMBOL_NOT_EQUALS = "!=";
-    static constexpr string_view SYMBOL_AND = "&&";
-    static constexpr string_view SYMBOL_OR = "||";
-    static constexpr char CHAR_OPEN_BRACKET = '(';
-    static constexpr char CHAR_CLOSE_BRACKET = ')';
+    // keywords
+    inline static string KEYWORD_PROCEDURE = "procedure";
+    inline static string KEYWORD_READ = "read";
+    inline static string KEYWORD_PRINT = "print";
+    inline static string KEYWORD_CALL = "call";
+    inline static string KEYWORD_WHILE = "while";
+    inline static string KEYWORD_IF = "if";
+    inline static string KEYWORD_THEN = "then";
+    inline static string KEYWORD_ELSE = "else";
+
+    // semicolon
+    inline static string SYMBOL_SEMICOLON = ";"; // terminal
+
+    // braces
+    inline static string SYMBOL_OPEN_BRACE = "{";  // terminal
+    inline static string SYMBOL_CLOSE_BRACE = "}"; // terminal
+
+    // brackets
+    inline static string SYMBOL_OPEN_BRACKET = "(";
+    inline static string SYMBOL_CLOSE_BRACKET = ")";
+
+    // assignment
+    inline static string SYMBOL_ASSIGN = "=";
+
+    // logical operators
+    inline static string SYMBOL_NOT = "!";
+    inline static string SYMBOL_AND = "&&";
+    inline static string SYMBOL_OR = "||";
+
+    // comparison operators
+    inline static string SYMBOL_LESS_THAN = "<";
+    inline static string SYMBOL_GREATER_THAN = ">";
+    inline static string SYMBOL_LESS_THAN_OR_EQUALS_TO = "<=";
+    inline static string SYMBOL_GREATER_THAN_OR_EQUALS_TO = ">=";
+    inline static string SYMBOL_EQUALS = "==";
+    inline static string SYMBOL_NOT_EQUALS = "!=";
+
+    // arithmetic operators
+    inline static string SYMBOL_PLUS = "+";
+    inline static string SYMBOL_MINUS = "-";
+    inline static string SYMBOL_MULTIPLY = "*";
+    inline static string SYMBOL_DIVIDE = "/";
+    inline static string SYMBOL_MODULO = "%";
+
+    // semicolon
+    static constexpr char CHAR_SEMICOLON = ';';
+
+    // braces
     static constexpr char CHAR_OPEN_BRACE = '{';
     static constexpr char CHAR_CLOSE_BRACE = '}';
+
+    // brackets
+    static constexpr char CHAR_OPEN_BRACKET = '(';
+    static constexpr char CHAR_CLOSE_BRACKET = ')';
+
+    // logical/comparison/assignment operators
     static constexpr char CHAR_ASSIGN = '=';
     static constexpr char CHAR_AMPERSAND = '&';
     static constexpr char CHAR_BAR = '|';
     static constexpr char CHAR_NOT = '!';
     static constexpr char CHAR_LESS_THAN = '<';
     static constexpr char CHAR_GREATER_THAN = '>';
+
+    // arithmetic operators
     static constexpr char CHAR_PLUS = '+';
     static constexpr char CHAR_MINUS = '-';
     static constexpr char CHAR_MULTIPLY = '*';
     static constexpr char CHAR_DIVIDE = '/';
     static constexpr char CHAR_MODULO = '%';
-    static constexpr char CHAR_SEMICOLON = ';';
 };
