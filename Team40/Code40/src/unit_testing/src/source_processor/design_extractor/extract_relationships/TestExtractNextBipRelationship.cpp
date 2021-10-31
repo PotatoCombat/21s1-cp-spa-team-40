6115c59891ab0a53464fab3b2f90324d2b9a5fc6
Wallace <wallacelim97@gmail.com>
2021-10-31 19:10:38 +0800

    Add unit test: Correctly extracts when a called procedure ends with an IfStatement

--- Team40/Code40/src/unit_testing/src/source_processor/design_extractor/extract_relationships/TestExtractNextBipRelationship.cpp
+++ Team40/Code40/src/unit_testing/src/source_processor/design_extractor/extract_relationships/TestExtractNextBipRelationship.cpp
@@ -1,3 +1,4 @@
+#include "../TestUtil.h"
 #include "catch.hpp"
 #include "pkb/PKB.h"
 #include "source_processor/design_extractor/DesignExtractor.h"
@@ -145,6 +146,88 @@ TEST_CASE("TestExtractNextBipRelationship: Correctly extracts p1, p2, p3 where "
                 .size() == 1);
     REQUIRE(TestExtractNextBipRelationship::pkb
                 .getNextBipLines(statement4.getIndex())
+                .count(statement2.getIndex()) == 1);
+
+    REQUIRE(TestExtractNextBipRelationship::pkb
+                .getNextBipLines(statement2.getIndex())
+                .empty());
+}
+TEST_CASE("TestExtractNextBipRelationship: Correctly extracts when a called "
+          "procedure ends with an IfStatement") {
+    TestExtractNextBipRelationship::reset();
+
+    Program program;
+    Procedure procedure1(TestExtractNextBipRelationship::PROC_NAME_1);
+    Procedure procedure2(TestExtractNextBipRelationship::PROC_NAME_2);
+    Statement statement1(1, StatementType::CALL);
+    Statement statement2(2, StatementType::READ);
+    Statement statement3(3, StatementType::IF);
+    Statement statement4(4, StatementType::READ);
+    Statement statement5(5, StatementType::READ);
+    Variable variable(TestExtractNextBipRelationship::VAR_NAME);
+
+    statement1.setProcName(procedure2.getName());
+    statement2.setVariable(&variable);
+    TestUtil::addConditionalExpression(&statement3);
+    statement4.setVariable(&variable);
+    statement5.setVariable(&variable);
+    statement3.addThenStmt(&statement4);
+    statement3.addElseStmt(&statement5);
+    procedure1.addToStmtLst(&statement1);
+    procedure1.addToStmtLst(&statement2);
+    procedure2.addToStmtLst(&statement3);
+    program.addToProcLst(&procedure1);
+    program.addToProcLst(&procedure2);
+
+    /**
+     * THE ABOVE PROGRAM TRANSLATES TO:
+     * ****************** START OF PROGRAM ******************
+     * procedure PROC_1 {
+     *    call PROC_2;           (1)
+     *    read x;                (2)
+     * }
+     * procedure PROC_2 {
+     *   if (x > 0) then {       (3)
+     *     read VAR;             (4)
+     *   } else {
+     *     read VAR;             (5)
+     *   }
+     * }
+     * ****************** END OF PROGRAM ******************
+     */
+
+    DesignExtractor de(&TestExtractNextBipRelationship::pkb);
+    de.extract(&program);
+
+    REQUIRE(TestExtractNextBipRelationship::pkb
+                .getNextBipLines(statement1.getIndex())
+                .size() == 1);
+    REQUIRE(TestExtractNextBipRelationship::pkb
+                .getNextBipLines(statement1.getIndex())
+                .count(statement3.getIndex()) == 1);
+
+    REQUIRE(TestExtractNextBipRelationship::pkb
+                .getNextBipLines(statement3.getIndex())
+                .size() == 2);
+    REQUIRE(TestExtractNextBipRelationship::pkb
+                .getNextBipLines(statement3.getIndex())
+                .count(statement4.getIndex()) == 1);
+    REQUIRE(TestExtractNextBipRelationship::pkb
+                .getNextBipLines(statement3.getIndex())
+                .count(statement5.getIndex()) == 1);
+
+    REQUIRE(TestExtractNextBipRelationship::pkb
+                .getNextBipLines(statement4.getIndex())
+                .size() == 1);
+    REQUIRE(TestExtractNextBipRelationship::pkb
+                .getNextBipLines(statement4.getIndex())
+                .count(statement2.getIndex()) == 1);
+
+    REQUIRE(TestExtractNextBipRelationship::pkb
+                .getNextBipLines(statement5.getIndex())
+                .size() == 1);
+    REQUIRE(TestExtractNextBipRelationship::pkb
+                .getNextBipLines(statement5.getIndex())
                 .count(statement2.getIndex()) == 1);
 
     REQUIRE(TestExtractNextBipRelationship::pkb
