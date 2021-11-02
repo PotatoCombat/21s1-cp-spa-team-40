d0b30431d7a58aed7829409eabaaeb2c278e65b1
Wallace <wallacelim97@gmail.com>
2021-11-02 15:37:29 +0800

    Use dfs instead of bfs in NextBip*

--- Team40/Code40/src/spa/src/query_processor/include/query_processor/relationship_handler/NextBipStarHandler.h
+++ Team40/Code40/src/spa/src/query_processor/include/query_processor/relationship_handler/NextBipStarHandler.h
@@ -8,21 +8,26 @@
 class NextBipStarHandler;
 
 typedef unordered_set<ProgLineIndex> (NextBipStarHandler::*ExplorationFunction)(
-    ProgLineIndex progLineIndex, vector<ProgLineIndex> &validBranchBackLines);
+    ProgLineIndex progLineIndex, vector<ProgLineIndex> &callStack);
 
 class NextBipStarHandler : public ClauseHandler {
 private:
     set<string> getR1ClauseR2(string r2) override;
     set<string> getR2ClausedR1(string r1) override;
     bool isR1ClauseR2(string r1, string r2) override;
-    set<string> breadthFirstSearch(ExplorationFunction explorationFunction,
-                                   const string &r);
+    void depthFirstSearch(ExplorationFunction explore, ProgLineIndex progLine,
+                          set<string> &result,
+                          unordered_set<ProgLineIndex> &visited,
+                          vector<ProgLineIndex> &validBranchLines);
     unordered_set<ProgLineIndex>
     getNextBipLines(ProgLineIndex progLineIndex,
                     vector<ProgLineIndex> &validBranchBackLines);
     unordered_set<ProgLineIndex>
     getPreviousBipLines(ProgLineIndex progLineIndex,
                         vector<ProgLineIndex> &validBranchInLines);
+
+    unordered_set<ProgLineIndex> getBranchInLines(ProgLineIndex branchBackFrom,
+                                                  ProgLineIndex branchBackTo);
 
 public:
     NextBipStarHandler(Clause *clause, PKB *pkb);
--- Team40/Code40/src/spa/src/query_processor/src/relationship_handler/NextBipStarHandler.cpp
+++ Team40/Code40/src/spa/src/query_processor/src/relationship_handler/NextBipStarHandler.cpp
@@ -8,47 +8,47 @@ NextBipStarHandler::NextBipStarHandler(Clause *clause, PKB *pkb)
     validRefType2 = &ClauseHandler::ALL_VALID_REF;
 }
 
-set<string> NextBipStarHandler::breadthFirstSearch(ExplorationFunction explore,
-                                                   const string &r) {
-    queue<ProgLineIndex> toExplore;
-    unordered_set<ProgLineIndex> visitedWhileStmts;
-    set<string> result;
+void NextBipStarHandler::depthFirstSearch(
+    ExplorationFunction explore, ProgLineIndex progLine, set<string> &result,
+    unordered_set<ProgLineIndex> &visited,
+    vector<ProgLineIndex> &validBranchLines) {
 
-    toExplore.push(stoi(r));
-
-    ProgLineIndex curIndex;
-    vector<ProgLineIndex> validBranchLines;
-    while (!toExplore.empty()) {
-        curIndex = toExplore.front();
-        toExplore.pop();
-        // If while statement, mark as visited to prevent infinite loop
-        if (pkb->getStmtByIndex(curIndex)->getStatementType() ==
-            StatementType::WHILE) {
-            visitedWhileStmts.insert(curIndex);
-        }
-
-        // Explore neighbours
-        for (ProgLineIndex index :
-             (this->*explore)(curIndex, validBranchLines)) {
-            if (visitedWhileStmts.find(index) == visitedWhileStmts.end()) {
-                toExplore.push(index);
-            }
-            result.insert(to_string(index));
-        }
+    // If while statement, mark as visited to prevent infinite loop
+    if (pkb->getStmtByIndex(progLine)->getStatementType() ==
+        StatementType::WHILE) {
+        visited.insert(progLine);
     }
 
-    return result;
+    // Explore neighbours
+    unordered_set<ProgLineIndex> toExplore =
+        (this->*explore)(progLine, validBranchLines);
+    for (ProgLineIndex index : toExplore) {
+        if (visited.find(index) == visited.end()) {
+            depthFirstSearch(explore, index, result, visited, validBranchLines);
+        }
+        result.insert(to_string(index));
+    }
+    if (!validBranchLines.empty() && validBranchLines.back() == progLine) {
+        validBranchLines.pop_back();
+    }
 }
 
 set<string> NextBipStarHandler::getR2ClausedR1(string r1) {
     ExplorationFunction explore = &NextBipStarHandler::getNextBipLines;
-    return breadthFirstSearch(explore, r1);
+    unordered_set<ProgLineIndex> visited;
+    vector<ProgLineIndex> validBranchLines;
+    set<string> result;
+    depthFirstSearch(explore, stoi(r1), result, visited, validBranchLines);
+    return result;
 }
 
 set<string> NextBipStarHandler::getR1ClauseR2(string r2) {
-    ExplorationFunction explorationFunction =
-        &NextBipStarHandler::getPreviousBipLines;
-    return breadthFirstSearch(explorationFunction, r2);
+    ExplorationFunction explore = &NextBipStarHandler::getPreviousBipLines;
+    unordered_set<ProgLineIndex> visited;
+    vector<ProgLineIndex> validBranchLines;
+    set<string> result;
+    depthFirstSearch(explore, stoi(r2), result, visited, validBranchLines);
+    return result;
 }
 
 bool NextBipStarHandler::isR1ClauseR2(string r1, string r2) {
@@ -91,18 +91,15 @@ unordered_set<ProgLineIndex> NextBipStarHandler::getNextBipLines(
         if (pkb->branchIn(curLine, nextBipLine)) {
             set<ProgLineIndex> branchBackLines = pkb->getNextLines(curLine);
             if (branchBackLines.size() > 1) {
-                throw runtime_error(
+                throw ClauseHandlerError(
                     "Encountered a call statement with more than one next "
                     "statement (syntactically impossible).");
             }
             validBranchBackLines.push_back(*branchBackLines.begin());
         } else if (pkb->branchBack(curLine, nextBipLine)) {
-            // If valid BranchBack, pop the stack
-            if (!validBranchBackLines.empty() &&
-                validBranchBackLines.back() == nextBipLine) {
-                validBranchBackLines.pop_back();
-            } else {
-                // Else, ignore it
+            // If invalid BranchBack, ignore it
+            if (validBranchBackLines.empty() ||
+                validBranchBackLines.back() != nextBipLine) {
                 continue;
             }
         }
@@ -118,24 +115,41 @@ unordered_set<ProgLineIndex> NextBipStarHandler::getPreviousBipLines(
     for (auto previousBipLine : previousBipLines) {
         // If BranchBack, add prev lines to validBranchInLines
         if (pkb->branchBack(previousBipLine, curLine)) {
-            set<ProgLineIndex> branchInLines = pkb->getPreviousLines(curLine);
-            if (branchInLines.size() > 1) {
-                throw runtime_error(
-                    "Encountered a call statement with more than one next "
-                    "statement (syntactically impossible).");
+            unordered_set<ProgLineIndex> branchInLines =
+                getBranchInLines(previousBipLine, curLine);
+            if (!branchInLines.empty()) {
+                validBranchInLines.push_back(*branchInLines.begin());
             }
-            validBranchInLines.push_back(*branchInLines.begin());
         } else if (pkb->branchIn(previousBipLine, curLine)) {
-            // If valid BranchIn, pop the stack
-            if (!validBranchInLines.empty() &&
-                validBranchInLines.back() == curLine) {
-                validBranchInLines.pop_back();
-            } else {
-                // Else, ignore it
+            // If invalid BranchIn, ignore it
+            if (validBranchInLines.empty() ||
+                validBranchInLines.back() != curLine) {
                 continue;
             }
         }
         validPreviousBipLines.insert(previousBipLine);
     }
     return validPreviousBipLines;
+}
+
+unordered_set<ProgLineIndex>
+NextBipStarHandler::getBranchInLines(ProgLineIndex branchBackFromLine,
+                                     ProgLineIndex branchBackToLine) {
+    unordered_set<ProgLineIndex> branchInLines;
+    Statement *branchBackFromStmt = pkb->getStmtByIndex(branchBackFromLine);
+    set<ProgLineIndex> prevLines = pkb->getPreviousLines(branchBackToLine);
+    for (auto prevLine : prevLines) {
+        Statement *prevStmt = pkb->getStmtByIndex(prevLine);
+        if (prevStmt->getStatementType() != StatementType::CALL) {
+            continue;
+        }
+        ProcName calledProcName = prevStmt->getProcName();
+        vector<Statement *> calledProcStmtLst =
+            pkb->getProcByName(calledProcName)->getStmtLst();
+        if (find(calledProcStmtLst.begin(), calledProcStmtLst.end(),
+                 branchBackFromStmt) != calledProcStmtLst.end()) {
+            branchInLines.insert(prevLine);
+        }
+    }
+    return branchInLines;
 }
